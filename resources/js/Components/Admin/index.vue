17009a07359277f236c74fb02c04476e50e98335
moji <mohammadsgr8@gmail.com>
2021-06-12 14:55:27 +0430

    [demo] == add a demo dashboard  to repo

---
+++ resources/js/Components/Admin/index.vue
@@ -0,0 +1,47 @@
+<template>
+<fragment>
+
+<Header />
+<Sidebar />
+<Main />
+<Footer />
+</fragment>
+
+
+</template>
+<script>
+import Vue from 'vue';
+import {Fragment} from 'vue-fragment';
+import Header from './Header.vue';
+import Sidebar from './Sidebar.vue';
+import Main from './Main.vue';
+import Footer from './Footer.vue';
+Vue.use(Fragment);
+export default{
+    name:'AdminDashboard',
+    components:{
+        Header,
+        Sidebar,
+        Main,
+        Footer,
+        Fragment
+        }
+}
+</script>
+
+
+<style>
+  @import '../../../css/vendor/bootstrap.min.css';
+  @import '../../../css/vendor/bootstrap.rtl.only.min.css';
+  @import '../../../css/vendor/fullcalendar.min.css';
+  @import '../../../css/vendor/dataTables.bootstrap4.min.css';
+  @import '../../../css/vendor/datatables.responsive.bootstrap4.min.css';
+  @import '../../../css/vendor/select2.min.css';
+  @import '../../../css/vendor/perfect-scrollbar.css';
+  @import '../../../css/vendor/glide.core.min.css';
+  @import '../../../css/vendor/bootstrap-stars.css';
+  @import '../../../css/vendor/nouislider.min.css';
+  @import '../../../css/vendor/bootstrap-datepicker3.min.css';
+  @import '../../../css/vendor/component-custom-switch.min.css';
+  @import '../../../css/main.css';
+</style>
--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,28 @@
-require('./bootstrap');
+// require('./bootstrap');
+
+window.Vue = require('vue').default;
+import App from './App.vue';
+import axios from 'axios';
+import VueAxios from 'vue-axios';
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+Vue.use(VueAxios, axios, VueRouter);
+
+// const routers = [{
+//     name: '/',
+//     path: '/',
+//     components: AdminDashboard
+// }];
+// const router = new VueRouter({ mode: 'history', router: routers });
+
+// const app = new Vue(
+//     Vue.util.extend(App)
+// ).$mount('#app');
+
+// const app = new Vue({
+//     el: '#app'
+// });
+
+new Vue({
+    render: h => h(App),
+}).$mount('#app');
