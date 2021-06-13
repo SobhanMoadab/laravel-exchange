007993305460221d24962de9996741b21e119907
Sobhab m <veiledassassin99@gmail.com>
2021-06-13 11:42:22 +0430

    CRUD FOR BLOG

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\BlogController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -19,6 +20,18 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+
+
 // A U T H E N T I C A T I ON
-    Route::post('/register', [AuthController::class, 'register']);
-    Route::post('/login', [AuthController::class, 'login']);
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::prefix('/dashboard')->middleware('auth:api')->group(function () {
+    // B L O G
+    Route::post('/blog', [BlogController::class, 'create_blog']);
+    Route::delete('/blog/{id}', [BlogController::class, 'delete_blog']);
+    Route::get('/blog/{p?}', [BlogController::class, 'get_blog']);
+    Route::put('/blog/{id}', [BlogController::class, 'edit_blog']);
+
+    
+});
