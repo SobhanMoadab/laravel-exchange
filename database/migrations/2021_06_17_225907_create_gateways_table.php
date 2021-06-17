38ac6a774d839b1c2627bb264dad8dc305680b8a
Moon Shadow <moon.shadow.rev@gmail.com>
2021-06-18 02:06:57 +0300

    Gateway Database Structure + Model

---
+++ database/migrations/2021_06_17_225907_create_gateways_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateGatewaysTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('gateways', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            // In This Method If Any Gateway Needed API Key Or Any Additional Configurations
+            // We Don't Need to Change Structure
+            $table->text('settings');
+            $table->boolean('is_active');
+            // Priority In Payment Page ( ex : Stripe : 1 , Paypal : 2 )
+            $table->integer('index');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('gateways');
+    }
+}
