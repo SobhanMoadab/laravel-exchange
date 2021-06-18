a65aedd11293be183ac45880b9fe0c76a43902ba
Moon Shadow <moon.shadow.rev@gmail.com>
2021-06-18 18:48:04 +0300

    [Add] : Sentry DSN

--- routes/api.php
+++ routes/api.php
@@ -21,24 +21,21 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-
-
-// A U T H E N T I C A T I ON
+// A U T H E N T I C A T I O N
 Route::post('/register', [AuthController::class, 'register'])->name('register');
 Route::post('/login', [AuthController::class, 'login'])->name('login');
-
-    // todo add middleware to check if user is registered, removed for developement reasons
+//TODO: add middleware to check if user is registered, removed for developement reasons
 Route::prefix('/dashboard')->group(function () {
     // B L O G
     Route::post('/post', [PostController::class, 'create_post'])->middleware('permission:store a post');
     Route::delete('/post/{id}', [PostController::class, 'delete_post'])->middleware('permission:delete a post');
     Route::get('/post/{p?}', [PostController::class, 'get_post']);
     Route::put('/post/{id}', [PostController::class, 'edit_post']);
-    // P E R M I S S I O N 
+    // P E R M I S S I O N
     Route::post('/create_role',[PermissionController::class, 'create_role']);
     Route::post('/create_subadmin',[PermissionController::class, 'create_subadmin']);
-    
 });
+
 Route::get('/debug-sentry', function () {
     throw new Exception('My first Sentry error!');
 });
