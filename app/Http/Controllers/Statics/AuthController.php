4e1f82f578777850a7092727a9d069c5e6246c54
Sobhab m <veiledassassin99@gmail.com>
2021-06-25 04:14:19 +0430

    [Up] : Static -> Statics

---
+++ app/Http/Controllers/Statics/AuthController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\Statics;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Routing\Controller;
+
+use Illuminate\Support\Facades\Auth;
+use App\Http\Controllers\Core\Services\AuthenticationServices;
+
+class AuthController extends Controller
+{
+
+
+}
