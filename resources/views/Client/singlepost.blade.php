3a2ef75332ded3ce55f3f7beeda8d220822a1c93
mohammad <mohammadsgr8@gmail.com>
2021-07-25 19:14:42 +0430

    [add] single post Client

---
+++ resources/views/Client/singlepost.blade.php
@@ -0,0 +1,45 @@
+@include('Client.Layout.Header')
+<section class="bg-home-2 align-items-center d-flex">
+
+    <div class="container">
+        <div class="row">
+
+
+            <div class="col-12">
+                <div class="card shadow-white">
+                    
+                    <div class="card-body">
+                        <div class=" d-flex justify-content-between  border-left mb-3">
+                            <div>
+                            <h5>       
+                                Single Post
+                            </h5>
+                            </div>
+                            <div>
+                                2021/2/10
+                            </div>
+                        </div>
+                        <div class="section">
+                            <div class="container ">
+                              
+                                    <div class="col-lg-12 ">
+                                            <p>
+                                                Lorem ipsum dolor sit amet 
+                                                consectetur adipisicing elit. Beatae nobis alias quaerat ratione iusto nulla fuga, corrupti asperiores sed, ipsa eaque sequi minus quidem eligendi iure molestias itaque ullam ab!
+                                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium, libero 
+                                                iste odio eos sed quis similique dicta illum velit, repellat quibusdam expedita facilis consectetur, quos voluptas! Odit eaque commodi delectus?
+                                                Lorem ipsum, dolor sit amet consectetur
+                                              
+                                            </p>
+                                        </div>
+
+                            </div>
+                        </div>
+                        <a class="btn btn-primary "  href="/"> Back To Site</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+@include('Client.Layout.Footer')
